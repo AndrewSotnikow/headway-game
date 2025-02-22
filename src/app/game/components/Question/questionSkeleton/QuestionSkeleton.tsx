0e89c8a123260204869724740e82f88a32288dbd
AndrewSotnikow <sot.shot@gmail.com>
2025-02-22 21:11:22 +0100

    update mobile styles

--- src/app/game/components/Question/questionSkeleton/QuestionSkeleton.tsx
+++ src/app/game/components/Question/questionSkeleton/QuestionSkeleton.tsx
@@ -5,8 +5,8 @@ export const Skeleton: React.FC = () => {
   return (
     <div className="skeleton">
       <div className="skeleton_leftContainer">
-        <div className="skeleton_question -mt133" />
-        <div className="skeleton_options -mb122">
+        <div className="skeleton_question -mt133 -mt100md" />
+        <div className="skeleton_options -mb122 -mb80md">
           <div className="skeleton_option"></div>
           <div className="skeleton_option"></div>
           <div className="skeleton_option"></div>
